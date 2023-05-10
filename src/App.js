83f5e032f85a57971ec1b0d3d97e2de7bb852f4a
Youssef Bedoui <Jozefbedoui@gmail.com>
2023-05-10 19:03:09 +0100

    deploy work

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/#/" element={<Home />} />
           <Route exact path="/movies" element={<MoviesScreen />} />
           <Route exact path="/tvShows" element={<TvShowsScreen />} />
           <Route path="/about_movie/:movieID/:type" element={<MovieInfo />} />
